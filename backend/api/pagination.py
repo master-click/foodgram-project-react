de6a4152d76b2654073e4675c626a86ad0d85f22
Ольга Батова <master-click@yandex.ru>
2022-12-10 15:03:46 +0300

    pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,4 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 6
+    def get_page_size(self, request):
+        if request.query_params.get('is_in_shopping_cart'):
+            return 999
+        return 6
